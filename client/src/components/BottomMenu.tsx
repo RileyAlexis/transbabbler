2ad66905937a768afa6b5502111248efb9a9e9c2
RileyAlexis <MxRileyAlexis@gmail.com>
2025-01-20 13:09:53 -0600

    added user menu back to bottom nav

--- client/src/components/BottomMenu.tsx
+++ client/src/components/BottomMenu.tsx
@@ -1,7 +1,7 @@
 import { GenerateBabbleButton } from "./GenerateBabbleButton";
 import { DataSetSelector } from "./DataSetSelector";
 import { ClearBabbleButton } from "./ClearBabbleButton";
-// import { UserMenu } from "./UserMenu";
+import { UserMenu } from "./UserMenu";
 
 export const BottomMenu: React.FC = () => {
     return (
@@ -10,8 +10,7 @@ export const BottomMenu: React.FC = () => {
                 <GenerateBabbleButton size="2" title="Babble" />
                 <DataSetSelector size="2" title="Data" />
                 <ClearBabbleButton size="2" title="Clear" />
-
-                {/* <UserMenu /> */}
+                <UserMenu />
             </div>
         </div>
     )
