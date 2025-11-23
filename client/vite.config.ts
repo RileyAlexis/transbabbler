04f8cd41ac60dbdd2c17c0c49581bfdb1154d790
RileyAlexis <MxRileyAlexis@gmail.com>
2025-11-23 11:47:54 -0600

    added vite-pwa plugin

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: "Transbabbler",
+        short_name: "Transbabbler",
+        start_url : "/",
+        display: "standalone",
+        background_color: "#0000",
+        theme_color: "#F5A9B8",
+        orientation: "portrait",
+        icons: [
+          
+        ]
+      },
+      workbox: {
+        navigateFallback: "/index.html",
+      }
+    })
+  ],
   server: {
     proxy: {
       '/api': {
