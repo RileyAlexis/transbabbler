4a47084db9c63b370d2ff08bd4e317359efe5641
RileyAlexis <MxRileyAlexis@gmail.com>
2026-04-09 18:16:23 -0500

    fixed common js import error causing bundle size to bloat

--- client/src/components/AdminPanel/Collections.tsx
+++ client/src/components/AdminPanel/Collections.tsx
@@ -5,7 +5,7 @@ import { useSelector } from "react-redux";
 import { Table } from "@radix-ui/themes";
 
 //Modules
-import { capitalize } from '../../modules/capitalize';
+import { capitalize } from "../../modules/capitalize";
 import { deleteWord } from "../../modules/deleteWord";
 import { updateWord } from "../../modules/updateWord";
 import { loadCollection } from "../../modules/loadCollection";
@@ -15,81 +15,100 @@ import { ModifyWord } from "./ModifyWord";
 import { BabbleCallout } from "../BabbleCallout";
 
 //Types
-import { Button } from "@radix-ui/themes/dist/cjs/index.js";
+import { Button } from "@radix-ui/themes";
 import { BabbleRootState } from "../../Types/BabblerRootState";
 
 interface CollectionsProps {
-    collection: string;
-    allWords: any[];
-    setAllWords: React.Dispatch<React.SetStateAction<string[]>>;
+  collection: string;
+  allWords: any[];
+  setAllWords: React.Dispatch<React.SetStateAction<string[]>>;
 }
 
+export const Collections: React.FC<CollectionsProps> = ({
+  collection,
+  allWords,
+  setAllWords,
+}) => {
+  const database = useSelector((state: BabbleRootState) => state.database);
+  const [loading, _setLoading] = useState(false);
+  const [isCalloutVisible, setIsCalloutVisible] = useState(false);
+  const [calloutMessage, setCalloutMessage] = useState("");
+  const [calloutColor, _setCalloutColor] = useState("blue");
 
-export const Collections: React.FC<CollectionsProps> = ({ collection, allWords, setAllWords }) => {
+  const handleLoadCollection = async () => {
+    const response = await loadCollection(
+      database.selectedDatabase,
+      collection,
+    );
+    setAllWords(response.data);
+  };
 
-    const database = useSelector((state: BabbleRootState) => state.database);
-    const [loading, _setLoading] = useState(false);
-    const [isCalloutVisible, setIsCalloutVisible] = useState(false);
-    const [calloutMessage, setCalloutMessage] = useState('');
-    const [calloutColor, _setCalloutColor] = useState('blue');
+  useEffect(() => {
+    handleLoadCollection();
+  }, [collection, database]);
 
+  const handleDeleteWord = async (word: string) => {
+    await deleteWord(database.selectedDatabase, collection, word);
+    setIsCalloutVisible(true);
+    setCalloutMessage(`${word} deleted from ${collection}`);
 
-    const handleLoadCollection = async () => {
-        const response = await loadCollection(database.selectedDatabase, collection);
-        setAllWords(response.data);
+    setTimeout(() => setIsCalloutVisible(false), 3000);
+    handleLoadCollection();
+  };
+
+  const handleModify = async (originalWord: string, modifiedWord: string) => {
+    if (originalWord === modifiedWord || modifiedWord === "") {
+      return;
+    } else {
+      await updateWord(
+        database.selectedDatabase,
+        collection,
+        modifiedWord,
+        originalWord,
+      );
+      handleLoadCollection();
     }
+  };
 
-    useEffect(() => {
-        handleLoadCollection();
-    }, [collection, database]);
+  return (
+    <div>
+      <Table.Root>
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>
+              {capitalize(collection)}
+            </Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Category</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Modify</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Delete</Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
 
+        <Table.Body>
+          {allWords.map((item, _id) => (
+            <Table.Row key={_id}>
+              <Table.RowHeaderCell>{item.word}</Table.RowHeaderCell>
+              <Table.Cell>{item.category}</Table.Cell>
 
-    const handleDeleteWord = async (word: string) => {
-        await deleteWord(database.selectedDatabase, collection, word);
-        setIsCalloutVisible(true);
-        setCalloutMessage(`${word} deleted from ${collection}`);
-
-        setTimeout(() => setIsCalloutVisible(false), 3000);
-        handleLoadCollection();
-    }
-
-    const handleModify = async (originalWord: string, modifiedWord: string) => {
-        if (originalWord === modifiedWord || modifiedWord === '') {
-            return;
-        } else {
-            await updateWord(database.selectedDatabase, collection, modifiedWord, originalWord);
-            handleLoadCollection();
-        }
-    }
-
-    return (
-        <div>
-            <Table.Root>
-                <Table.Header>
-                    <Table.Row>
-                        <Table.ColumnHeaderCell>{capitalize(collection)}</Table.ColumnHeaderCell>
-                        <Table.ColumnHeaderCell>Category</Table.ColumnHeaderCell>
-                        <Table.ColumnHeaderCell>Modify</Table.ColumnHeaderCell>
-                        <Table.ColumnHeaderCell>Delete</Table.ColumnHeaderCell>
-
-                    </Table.Row>
-                </Table.Header>
-
-                <Table.Body>
-                    {allWords.map((item, _id) => (
-                        <Table.Row key={_id}>
-                            <Table.RowHeaderCell>{item.word}</Table.RowHeaderCell>
-                            <Table.Cell>{item.category}</Table.Cell>
-
-                            <Table.Cell>
-                                <ModifyWord word={item.word} loading={loading} onSubmit={(word: string) => handleModify(item.word, word)} />
-                            </Table.Cell>
-                            <Table.Cell><Button onClick={() => handleDeleteWord(item.word)}>X</Button></Table.Cell>
-                        </Table.Row>
-                    ))}
-                </Table.Body>
-            </Table.Root>
-            <BabbleCallout message={calloutMessage} color={calloutColor} isVisible={isCalloutVisible} />
-        </div>
-    )
-}
+              <Table.Cell>
+                <ModifyWord
+                  word={item.word}
+                  loading={loading}
+                  onSubmit={(word: string) => handleModify(item.word, word)}
+                />
+              </Table.Cell>
+              <Table.Cell>
+                <Button onClick={() => handleDeleteWord(item.word)}>X</Button>
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
+      <BabbleCallout
+        message={calloutMessage}
+        color={calloutColor}
+        isVisible={isCalloutVisible}
+      />
+    </div>
+  );
+};
